dfcadef6a3c003d489ca1acc4b38a84d2cd221bb
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-02-08 20:03:10 +0530

    Update itd.js

--- routes/itd.js
+++ routes/itd.js
@@ -2,18 +2,16 @@ const express = require('express');
 const router = express.Router();
 const itelugu=require("../models/ite")
 
-var data=[]
-itelugu.find((err,dat)=>{
-    var datas=Array.from(dat)
-    data.push(datas)
-    data=[data[data.length-1]]
-})
-
-
 
 router.get("/",(req,res)=>{
-    res.render('details.ejs',{
-        data:data,
+    var data=[]
+    itelugu.find((err,dat)=>{
+        var datas=Array.from(dat)
+        data.push(datas)
+        data=[data[data.length-1]]
+        res.render('details.ejs',{
+            data:data,
+        })
     })
 })
 
