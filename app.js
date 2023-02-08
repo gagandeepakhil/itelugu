31722a6e7dbc96e2c26031617b7c162a5f3c555e
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-02-08 17:29:54 +0530

    init

---
+++ app.js
@@ -0,0 +1,117 @@
+const express=require("express");
+const bp=require("body-parser");
+const app=express();
+const mongoose = require('mongoose');
+const alert=require('alert')
+
+app.set("view engine","ejs");
+app.use(bp.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+
+const dbURI="mongodb+srv://gagan:gagan@cluster.xdg5ro5.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result =>{ 
+    const port = process.env.PORT || 3000;
+    app.listen(port)})
+  .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const dataSchema = new Schema({
+    
+        purpose: {
+            type:String,
+        },
+        name: {
+            type:String,
+        },
+        affiliation: {
+            type:String,
+        },
+        insti: {
+            type:String,
+        },
+        age: {
+            type:String,
+        },
+        sex: {
+            type:String,
+        },
+        city: {
+            type:String,
+        },
+        number: {
+            type:String,
+        },
+        mail: {
+            type:String,
+        },
+        require: {
+            type:String,
+        },
+        arrivaldate: {
+            type:String,
+        },
+        arrivaltime:{
+            type:String,
+        },
+        mode: {
+            type:String,
+        },
+        departuredate: {
+            type:String,
+        },
+        departuretime:{
+            type:String,
+        },
+      
+}, { timestamps: true });
+
+const itelugu = mongoose.model('itelugu', dataSchema);
+var data=[]
+function sendD(itelugu){
+    itelugu.find((err,dat)=>{
+        var datas=Array.from(dat)
+        data.push(datas)
+        data=[data[data.length-1]]
+   })
+}
+function suc(){
+    alert("data submitted succesfully :)")
+}   
+app.get("/",function(req,res){
+    
+   res.render('index.ejs',{})
+    
+})
+
+
+
+app.get("/details",function(req,res){
+    sendD(itelugu)
+    res.render('details.ejs',{
+        data:data,
+    })
+     
+ })
+
+app.post("/",function(req,res){
+    var data=req.body;
+    const blog = new itelugu(data);
+    blog.save()
+        .then(result => {
+        console.log(result);
+        suc()
+        res.redirect("/");
+        })
+        .catch(err => {
+        console.log(err);
+        });
+        
+    
+}) 
+
+app.listen(3050,function(){
+    console.log("hi");
+})
---
+++ public/script.js
@@ -0,0 +1,169 @@
+// collapsible menu
+butt=Array.from(document.body.getElementsByClassName('info'))
+butt.forEach(ele=>{
+    ele.querySelector('button').addEventListener('click',function(){
+        ele.querySelector('button').getElementsByClassName('contents')[0].classList.toggle('c1')
+    })
+})
+
+//carousel for pc
+$(function(){
+    $('.car-item').eq(0).addClass('active');
+    var total = $('.car-item').length;
+    var current = 0;
+    $('#moveRight').on('click', function(){
+      var next=current;
+      current= current+1;
+      setSlide(next, current);
+    });
+    $('#moveLeft').on('click', function(){
+      var prev=current;
+      current = current- 1;
+      setSlide(prev, current);
+    });
+    function setSlide(prev, next){
+      var slide= current;
+      if(next>total-1){
+       slide=0;
+        current=0;
+      }
+      if(next<0){
+        slide=total - 1;
+        current=total - 1;
+      }
+             $('.car-item').eq(prev).removeClass('active');
+             $('.car-item').eq(slide).addClass('active');
+        setTimeout(function(){
+  
+        },800);
+      
+  
+      
+      console.log('current '+current);
+      console.log('prev '+prev);
+    }
+  });
+
+  //guests
+  function on1() {
+    document.getElementById("guestinfo1").style.display = "block";
+  }
+  
+  function off1() {
+    document.getElementById("guestinfo1").style.display = "none";
+  }
+  function on2() {
+    document.getElementById("guestinfo2").style.display = "block";
+  }
+  
+  function off2() {
+    document.getElementById("guestinfo2").style.display = "none";
+  }
+  function on3() {
+    document.getElementById("guestinfo3").style.display = "block";
+  }
+  
+  function off3() {
+    document.getElementById("guestinfo3").style.display = "none";
+  }
+  function on4() {
+    document.getElementById("guestinfo4").style.display = "block";
+  }
+  
+  function off4() {
+    document.getElementById("guestinfo4").style.display = "none";
+  }
+  function on5() {
+    document.getElementById("guestinfo5").style.display = "block";
+  }
+  
+  function off5() {
+    document.getElementById("guestinfo5").style.display = "none";
+  }
+  function on6() {
+    document.getElementById("guestinfo6").style.display = "block";
+  }
+  
+  function off6() {
+    document.getElementById("guestinfo6").style.display = "none";
+  }
+  function on7() {
+    document.getElementById("guestinfo7").style.display = "block";
+  }
+  
+  function off7() {
+    document.getElementById("guestinfo7").style.display = "none";
+  }
+
+  // guest house
+
+  function on() {
+    if(document.body.getElementsByTagName('select')[0].value=="participating"){
+      document.getElementsByClassName("guesth")[0].style.display = "block";
+      document.getElementById("purpose").value="participating"
+    }
+    
+    if(document.body.getElementsByTagName('select')[0].value=="visiting"){
+      document.getElementsByClassName("guesth")[0].style.display = "none";
+      document.getElementById("purpose").value="visiting"
+    }
+    
+  }
+
+
+  function sc(){
+    if(document.body.getElementsByTagName('select')[1].value=="Male")
+    document.getElementById("sex").value="Male"
+    if(document.body.getElementsByTagName('select')[1].value=="Female")
+    document.getElementById("sex").value="Female"
+    if(document.body.getElementsByTagName('select')[1].value=="Others")
+    document.getElementById("sex").value="Others"
+  }
+
+  function modesel(){
+    
+      if(document.body.getElementsByTagName('select')[3].value=="Air")
+      document.getElementById("mode").value="Air"
+      if(document.body.getElementsByTagName('select')[3].value=="Bus")
+      document.getElementById("mode").value="Bus"
+      if(document.body.getElementsByTagName('select')[3].value=="Train")
+      document.getElementById("mode").value="Train"
+      if(document.body.getElementsByTagName('select')[3].value=="Others")
+      document.getElementById("mode").value="Others"
+  
+  }
+
+  function checks(){
+    if(document.body.getElementsByTagName('select')[2].value=="Yes"){
+      document.getElementsByClassName("gues")[0].style.display = "block";
+      document.getElementById("require").value="Yes"
+    }
+    if(document.body.getElementsByTagName('select')[2].value=="No"){
+      document.getElementsByClassName("gues")[0].style.display = "none";
+      document.getElementById("require").value="No"
+    }
+  }
+
+//cards events
+
+  const c=Array.from(document.getElementsByClassName('cardi')) ;
+c.forEach(ele=>(
+    ele.addEventListener('mouseover',function(){
+        ele.classList.toggle('is-flipped');
+    })
+));
+
+//support us
+
+const pay=document.getElementsByClassName('pay')[0];
+document.getElementById('yes').addEventListener('click',function(){
+  pay.style.display="block";
+  pay.classList.add("slidedown");
+});
+document.getElementById('no').addEventListener('click',function(){
+    pay.classList.remove("slidedown");
+    pay.classList.add("slideup");
+    setTimeout(()=>{
+        pay.style.display="none";
+    },1000)
+  });
