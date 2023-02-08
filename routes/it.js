5135b427a3e91df5fbcd7f986be5fdc04b201eb8
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-02-08 18:32:32 +0530

    Update it.js

--- routes/it.js
+++ routes/it.js
@@ -3,11 +3,10 @@ const itcontrol = require('../controller/control');
 const router = express.Router();
 
 
-
+router.get("/details",itcontrol.it_details)
 router.get("/",function(req,res){
    res.render('index',{})
 })
-router.get("/details",itcontrol.it_details)
 router.post("/",itcontrol.send_details) 
 
 module.exports=router;
