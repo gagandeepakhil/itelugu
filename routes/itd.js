0b151c564f49d94d966a73879b490177d338cc12
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-02-14 23:16:54 +0530

    Update itd.js

--- routes/itd.js
+++ routes/itd.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const itelugu=require("../models/ite")
-
+const mongoose = require('mongoose');
+const dbURI="mongodb+srv://gagan:gagan@cluster.xdg5ro5.mongodb.net/?retryWrites=true&w=majority"
 
 router.get("/",(req,res)=>{
+    mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result =>{ 
     var da=[]
     itelugu.find((err,dat)=>{
         var datas=Array.from(dat)
@@ -13,6 +16,9 @@ router.get("/",(req,res)=>{
             data:da,
         })
     })
+  })
+  .catch(err => console.log(err));
+   
 })
 
 module.exports=router;
