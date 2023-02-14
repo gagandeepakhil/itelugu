8cdee66ba42d6907f3cf03a5c5eba07fb990e89d
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-02-14 23:10:40 +0530

    fi

--- controller/control.js
+++ controller/control.js
@@ -1,7 +1,10 @@
 const itelugu=require("../models/ite")
-
+const mongoose = require('mongoose');
+const dbURI="mongodb+srv://gagan:gagan@cluster.xdg5ro5.mongodb.net/?retryWrites=true&w=majority"
 
 const send_details=(req,res)=>{
+    mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result =>{ 
     var data=req.body;
     const blog = new itelugu(data);
     blog.save()
@@ -12,6 +15,8 @@ const send_details=(req,res)=>{
         .catch(err => {
         console.log(err);
         });
+  })
+  .catch(err => console.log(err));
 }
 
 module.exports={
